510b6ee1c432ddcd21427c88e7764b689c70351a
root <tanguy.bonneau@gmail.com>
2020-02-06 19:38:40 +0100

    Improve config page

--- frontend/teleinfo.config.js
+++ frontend/teleinfo.config.js
@@ -8,7 +8,32 @@ var teleinfoConfigDirective = function(teleinfoService, raspiotService) {
     {
         var self = this;
         self.port = '';
-        self.teleinfo = {};
+        self.teleinfo = [];
+        self.extra = {
+            'ADCO':     {'label': 'Adresse du concentrateur de téléreport', 'unit': ''},
+            'OPTARIF':  {'label': 'Option tarifaire choisie', 'unit': ''},
+            'BASE':     {'label': 'Index option base', 'unit': 'Wh'},
+            'HCHC':     {'label': 'Index option heures creuses', 'unit': 'Wh'},
+            'HCHP':     {'label': 'Index option heures pleines', 'unit': 'Wh'},
+            'EJPHN':    {'label': 'Index option EJP heures normales', 'unit': 'Wh'},
+            'EJPHPN':   {'label': 'Index option EJP heures de pointe mobile', 'unit': 'Wh'},
+            'PTEC':     {'label': 'Période tarifaire en cours', 'unit': ''},
+            'MOTDETAT': {'label': 'Mot d\'état du compteur', 'unit': ''},
+            'ISOUSC':   {'label': 'Intensité souscrite', 'unit': 'A'},
+            'BBRHCJB':  {'label': 'Index option tempo heures creuses jours bleus', 'unit': 'Wh'},
+            'BBRHPJB':  {'label': 'Index option tempo heures pleines jours bleus', 'unit': 'Wh'},
+            'BBRHCJW':  {'label': 'Index option tempo heures creuses jours blancs', 'unit': 'Wh'},
+            'BBRHPJW':  {'label': 'Index option tempo heures pleines jours blancs', 'unit': 'Wh'},
+            'BBRHCJR':  {'label': 'Index option tempo heures creuses jours rouges', 'unit': 'Wh'},
+            'BBRHPJR':  {'label': 'Index option tempo heures pleines jours rouges', 'unit': 'Wh'},
+            'PEJP':     {'label': 'Préavis début EJP (30min)', 'unit': 'min'},
+            'DEMAIN':   {'label': 'Couleur du lendemain', 'unit': ''},
+            'IINST':    {'label': 'Intensité instantanée', 'unit': 'A'},
+            'ADPS':     {'label': 'Avertissement de dépassement de puissance souscrite', 'unit': 'A'},
+            'IMAX':     {'label': 'Intensité maximale appelée', 'unit': 'A'},
+            'HHPHC':    {'label': 'Horaires heures pleines heures creuses', 'unit': ''},
+            'PAPP':     {'label': 'Puissance apparente', 'unit': 'VA'},
+        };
 
         /**
          * Init controller
@@ -30,8 +55,15 @@ var teleinfoConfigDirective = function(teleinfoService, raspiotService) {
          */
         self.getTeleinfo = function() {
             teleinfoService.getTeleinfo()
-                .then(function(data) {
-                    self.teleinfo = data;
+                .then(function(resp) {
+                    for(var index in resp.data) {
+                        self.teleinfo.push({
+                            'key': resp.data[index].key,
+                            'value': resp.data[index].value,
+                            'unit': self.extra[resp.data[index].key].unit,
+                            'label': self.extra[resp.data[index].key].label,
+                        });
+                    }
                 });
         };
 
